c0c98fda97d8325a0c55c25e322770444996a3d8
Bohdan <bohdan.verbetskyi@gmail.com>
2025-11-12 19:24:02 +0100

    Add middleware

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = require('./routers');
+const { errorHandlers } = require('./middleware');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api', router);
+
+app.use(errorHandlers.dbErrorHandler, errorHandlers.errorHandler);
 
 module.exports = app;
